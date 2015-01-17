c95e84a6defe286844ad93d158fdd07d1cdb7efa
sean1rose <sean1rose@gmail.com>
2015-01-17 12:35:49 -0800

    Cleaned up lost.controller code --> moved to gmapfactory service

--- client/app/gMapFactory/gMapFactory.service.js
+++ client/app/gMapFactory/gMapFactory.service.js
@@ -5,47 +5,61 @@ angular.module('lostPawsApp')
     // Service logic
 
     var geocodeService = {};
-    var baseUrl = 'http://maps.google.com/maps/api/geocode/json?address=';
-    var _address = '';
-    var _finalUrl = '';
 
-    // _address = singlePet.addressFound
-    var makeUrl = function(){
-      console.log('in makeUrl');
-      _address = _address.split(' ').join('+');
-      _finalUrl = baseUrl + _address + '&sensor=false';
-      return _finalUrl;
+    geocodeService.displayMap = function(standardAddress, map){
+      var geocoder = new google.maps.Geocoder();
+      geocoder.geocode( {'address': standardAddress}, function(results, status){
+        if (status == google.maps.GeocoderStatus.OK){
+          console.log('results are ', results);
+          map.setCenter(results[0].geometry.location);
+        } else {
+          console.log('displayMap service func did not work cuz ', status);
+        }
+      })
     };
 
-    // going to pass in singlePet.addressFound
-    geocodeService.setAddress = function(address){
-      _address = address;
-    }
-
-    geocodeService.getAddress = function(){
-      return _address;
-    }
-
-    geocodeService.callGMaps = function(){
-      console.log('in service + callGMaps');
-      makeUrl();
-      var deferred = $q.defer();
-      
-      // cant get request the URL *HERE* after logging in - cros domain CORS issue / No Access-Control-Allow-Origin / SOP - Same Origin Policy
-      $http.get(_finalUrl).success(function(data){
-        console.log('data is ', data);
-        deferred.resolve(data);
-      }).error(function(){
-        deferred.reject('Error occurred!');
+    geocodeService.parsePetSearch = function(location, map){
+      var geocoder = new google.maps.Geocoder();
+      // parsing in order to retrieve formatted address for input to displayMap...
+      geocoder.geocode( {'address': location}, function(results, status){
+        if (status == google.maps.GeocoderStatus.OK){
+          console.log('in the parsePetSearch geocoder');
+          var coordObj = results[0].geometry.location;
+          var lati = coordObj.D;
+          var longi = coordObj.k;
+          var regAddress = results[0].formatted_address;
+          console.log('address is ', regAddress);
+          // call displayMap w/ formatted address
+          geocodeService.displayMap(regAddress, map);
+        } else {
+          console.log('parsePetSearch failed! cuz ', status);
+        }
       })
-      return deferred.promise;
-    }
+    };
 
-    // Public API here
+    geocodeService.markerCreator = function(arrayOfPets, map){
+      var geocoder = new google.maps.Geocoder();
+      console.log('inside markerCreator service function, we have an array of pets from the db: ', arrayOfPets);
+      arrayOfPets.forEach(function(item, index, array){
+        var singlePet = item;
+        var petName = singlePet.name;
+        var petType = singlePet.type;
+        var petDate = singlePet.dateFound;
+        var location = singlePet.addressFound;
+        geocoder.geocode( { 'address': location}, function(results, status){
+          if (status == google.maps.GeocoderStatus.OK){
+            console.log('markerCreator service func status is A-OK, see: ', results);
+            var regularAddress = results[0].formatted_address;
+            var marker = new google.maps.Marker({
+              map: map,
+              position: results[0].geometry.location
+            });
+            console.log('marker: ', marker);
+          }
+        })
+      })
+    };
+
     return geocodeService;
-    // return {
-    //   someMethod: function () {
-    //     return geocodeService;
-    //   }
-    // };
+
   });
--- client/app/lost/lost.controller.js
+++ client/app/lost/lost.controller.js
@@ -24,7 +24,7 @@ angular.module('lostPawsApp')
       socket.unsyncUpdates('pet');
     });
 
-
+    // initial setting config of g-map
     var latlng = new google.maps.LatLng(37.7833, -122.4167);
     
     var mapOptions = {
@@ -35,167 +35,21 @@ angular.module('lostPawsApp')
     // initial Initialization of map
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-    var displayMap = function(standardAddress){
-      var geocoder = new google.maps.Geocoder();
-      // var mapOptions = {
-      //   center: { lat: latit, lng: longit},
-      //   zoom: 15
-      // }
-      geocoder.geocode( {'address': standardAddress}, function(results, status){
-        if (status == google.maps.GeocoderStatus.OK){
-          console.log('results are ', results);
-          map.setCenter(results[0].geometry.location);
-        } else {
-          console.log('map and geocoder did not display cuz ', status);
-        }
-      })
-    }
-
-
-    // model for find pet search box (user inputs address/zip and map centers to that location)
+    // Lost Pet Search that displays a map centered at searched address
     $scope.findPet = function(){
       var location = $scope.formInfo.location;
       $scope.formInfo.location = '';
-
-      var geocoder = new google.maps.Geocoder();
-      geocoder.geocode( { 'address': location}, function(results, status){
-        if (status == google.maps.GeocoderStatus.OK){
-          console.log('in the findPets geocoder');
-          var coordObj = results[0].geometry.location;
-          var lati = coordObj.D;
-          var longi = coordObj.k;
-          var regAddress = results[0].formatted_address;
-          console.log('address is ', regAddress);
-          displayMap(regAddress);
-        } else {
-          console.log('pet search failed!');
-        }
-      })
-
-
-      // gMapFactory.setAddress(location);
-      // gMapFactory.callGMaps().then(function(data){
-      //   var petLatitude = data.results[0].geometry.location.lat;
-      //   var petLongitude = data.results[0].geometry.location.lng;
-      //   $scope.map = {
-      //     center: {
-      //       latitude: petLatitude,
-      //       longitude: petLongitude
-      //     },
-      //     zoom: 15
-      //   };
-      // });
+      gMapFactory.parsePetSearch(location, map);
     };
 
     $scope.petMarkers = [];
 
+    // accessing pets API to retrieve pets and display markers
     $http.get('/api/pets').success(function(foundPets){
       $scope.foundPets = foundPets;
       var listOfPets = $scope.foundPets;
-      var markerCreator = function(arrayOfPets){
-        var geocoder = new google.maps.Geocoder();
-        console.log('inside markerCreator funciton, have an array of pets from db - ', arrayOfPets);
-        arrayOfPets.forEach(function(item, index, array){
-          var singlePet = item;
-          var petName = singlePet.name;
-          var petType = singlePet.type;
-          var location = singlePet.addressFound;
-          geocoder.geocode( { 'address': location}, function(results, status){
-            if (status == google.maps.GeocoderStatus.OK){
-              console.log('status is GOOD!, see!!! --- ', results);
-              // console.log('retrieve address correctly? --> ', results[0].formatted_address);
-              var polePosition = results[0].formatted_address;
-              // var petLatitude = results[0].geometry.location.lat;
-              // var petLongitude = results[0].geometry.location.lng;
-
-              var petLatitude = results[0].geometry.location.D;
-              var petLongitude = results[0].geometry.location.k;
-              console.log('petLatitude ', petLatitude);
-              console.log('petLongitude ', petLongitude);
-              var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location
-              });
-              console.log('marker is ', marker);
-            }
-          })
-        })
-      }
-      markerCreator(listOfPets);
+      gMapFactory.markerCreator(listOfPets, map);
     });
 
 
   });
-
-    // default google maps 
-    // $scope.map = {
-    //   center: {
-    //     latitude: 37.7833,
-    //     longitude: -122.4167
-    //   },
-    //   zoom: 15,
-    //   bounds: {}
-    // };
-
-              // var obj = {
-              //   latitude: petLatitude,
-              //   longitude: petLongitude,
-              //   title: petName,
-              //   title2: petType,
-              //   id: index,
-              //   show: false,
-              //   onClick: function(){
-              //     console.log('Marker clicked');
-              //     obj.show = !obj.show;
-              //   }
-              // };
-              // console.log('obj IS ', obj);
-              // $scope.$watch(function(){
-              //   return $scope.map.bounds;
-              // }, function(){
-              //   var markers = [];
-              //   $scope.petMarkers.push(obj);
-              //   console.log('petMarkers IS ', $scope.petMarkers);
-              // }, true);
-
-    // // PURPOSE: display each pet in database to map + add markers/windows
-    // // get the pet-database data from API, upon success...
-    // $http.get('/api/pets').success(function(foundPets){
-    //   $scope.foundPets = foundPets;
-    //   var listOfPets = $scope.foundPets;
-    //   console.log('list! ', listOfPets);
-    //   var markerCreator = function(arrayOfPets){
-    //     console.log('markerCreator is called! arrayOfPets is - ', arrayOfPets);
-    //     arrayOfPets.forEach(function(item, index, array){
-    //       var singlePet = item;
-    //       var petName = singlePet.name;
-    //       var petType = singlePet.type;
-    //       var location = singlePet.addressFound  
-    //       // incorporate factory
-    //       gMapFactory.setAddress(location);
-    //       gMapFactory.callGMaps().then(function(data){
-    //         var petLatitude = data.results[0].geometry.location.lat;
-    //         var petLongitude = data.results[0].geometry.location.lng;
-    //         var obj = {
-    //           latitude: petLatitude,
-    //           longitude: petLongitude,
-    //           title: petName,
-    //           title2: petType,
-    //           id: index,
-    //           show: false,
-    //           onClick: function(){
-    //             console.log("Clicked");
-    //             obj.show = !obj.show;
-    //           }
-    //         };
-    //         $scope.$watch(function(){
-    //           return $scope.map.bounds;
-    //         }, function(){
-    //           var markers = [];
-    //           $scope.petMarkers.push(obj);
-    //         }, true);
-    //       });
-    //     });
-    //   };
-    //   markerCreator(listOfPets);
-    // });
